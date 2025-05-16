5636613d754257a9f10c717304e797fb85c8213d
Kai Gülzau <kguelzau@novomind.com>
2025-05-16 14:07:16 +0200

    reproduce https://github.com/primefaces/primefaces/issues/13792

--- src/main/java/org/primefaces/test/TestObject.java
+++ src/main/java/org/primefaces/test/TestObject.java
@@ -1,6 +1,8 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,7 +15,10 @@ public class TestObject implements Serializable {
     private String name;
     private String artist;
     private Integer released;
-
+    private List<String> images = new ArrayList<>(List.of(
+            "https://www.primefaces.org/showcase/javax.faces.resource/images/home/jsf-components.png.xhtml?ln=showcase",
+            "https://www.primefaces.org/showcase/javax.faces.resource/images/home/jsf-opensource.png.xhtml?ln=showcase"
+    ));
     public TestObject(String name, String artist, Integer released) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
--- src/main/webapp/resources/test.js
+++ src/main/webapp/resources/test.js
@@ -1,1 +1,13 @@
 /* Place any Javascript here you want loaded in test.xhtml */
+
+/* workaround
+PrimeFaces.widget.Galleria = PrimeFaces.widget.Galleria.extend({
+    show: function (index) {
+        // disable animation because "onEntering" is not called reliable
+        let state = PrimeFaces.animationEnabled;
+        PrimeFaces.animationEnabled = false;
+        this._super(index);
+        PrimeFaces.animationEnabled = state;
+    }
+});
+*/
